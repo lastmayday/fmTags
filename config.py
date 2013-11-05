bb2f69c6f43e34e90d72e1bde02c19d57f6865ac
root <root@bingyan13.hustonline.net>
2013-11-05 13:21:41 +0800

    config

--- config.py
+++ config.py
@@ -7,4 +7,3 @@ BROKEN_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 SECRET_KEY = 'not_a_secret'
-
--- manage.py
+++ manage.py
@@ -1,4 +1,11 @@
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from app import app
 
+import sys
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(int(sys.argv[1]))
+    IOLoop.instance().start()
